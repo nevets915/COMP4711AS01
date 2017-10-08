9cda84b291c933b648c075a585b6bed73b396f3d
George <namblue@me.com>
2017-10-07 17:20:25 -0700

    Initial Change to template webpage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,9 +1,164 @@
-<div class="row">
-    {authors}
-	<div class="span4">
-		<a href="/welcome/show/{key}">
-			<img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-		</a>
-	</div>
-    {/authors}
+<div class="for_banners">
+  <article class="col1">
+    <div class="tabs">
+      <ul class="nav">
+        <li class="selected"><a href="#Flight">Flight</a></li>
+        <li><a href="#Hotel">Hotel</a></li>
+        <li class="end"><a href="#Rental">Rental</a></li>
+      </ul>
+      <div class="content">
+        <div class="tab-content" id="Flight">
+          <form id="form_1" action="#" method="post">
+            <div>
+              <div class="radio">
+                <div class="wrapper">
+                  <input type="radio" name="name1" checked>
+                  <span class="left">Standard</span>
+                  <input type="radio" name="name1">
+                  <span class="left">World Map</span> </div>
+              </div>
+              <div class="row"> <span class="left">From</span>
+                <input type="text" class="input">
+              </div>
+              <div class="row"> <span class="left">To</span>
+                <input type="text" class="input">
+              </div>
+              <div class="wrapper">
+                <div class="col1">
+                  <div class="row"> <span class="left">Outbound</span>
+                    <input type="text" class="input1" value="03.05.2011"  onblur="if(this.value=='') this.value='03.05.2011'" onFocus="if(this.value =='03.05.2011' ) this.value=''">
+                  </div>
+                  <div class="row"> <span class="left">Return</span>
+                    <input type="text" class="input1" value="10.05.2011"  onblur="if(this.value=='') this.value='10.05.2011'" onFocus="if(this.value =='10.05.2011' ) this.value=''">
+                  </div>
+                </div>
+                <input type="text" class="input1 marg_top1" value="+/- 0 Days"  onblur="if(this.value=='') this.value='+/- 0 Days'" onFocus="if(this.value =='+/- 0 Days' ) this.value=''">
+              </div>
+              <div class="row"> <span class="left">Adults</span>
+                <input type="text" class="input2" value="2"  onblur="if(this.value=='') this.value='2'" onFocus="if(this.value =='2' ) this.value=''">
+              </div>
+              <div class="row"> <span class="left">Children</span>
+                <input type="text" class="input2" value="0"  onblur="if(this.value=='') this.value='0'" onFocus="if(this.value =='0' ) this.value=''">
+                <span class="pad_left1">(0-11 years)</span> </div>
+              <div class="wrapper"> <span class="right relative"><a href="#" class="button1"><strong>Search</strong></a></span> <a href="#" class="link1">More Options</a> </div>
+            </div>
+          </form>
+        </div>
+        <div class="tab-content" id="Hotel">
+          <form id="form_2" action="#" method="post">
+            <div>
+              <div class="radio">
+                <div class="wrapper">
+                  <input type="checkbox" checked>
+                  Our Partners </div>
+              </div>
+              <div class="row"> <span class="left">Location</span>
+                <input type="text" class="input">
+              </div>
+              <div class="row"> <span class="left">Check-in </span>
+                <input type="text" class="input1" value="03.05.2011"  onblur="if(this.value=='') this.value='03.05.2011'" onFocus="if(this.value =='03.05.2011' ) this.value=''">
+                <a href="#" class="help"></a> </div>
+              <div class="row"> <span class="left">Check-out </span>
+                <input type="text" class="input1" value="10.05.2011"  onblur="if(this.value=='') this.value='10.05.2011'" onFocus="if(this.value =='10.05.2011' ) this.value=''">
+                <a href="#" class="help"></a> </div>
+              <div class="row"> <span class="left">Rooms</span>
+                <input type="text" class="input2" value="1"  onblur="if(this.value=='') this.value='1'" onFocus="if(this.value =='1' ) this.value=''">
+                <a href="#" class="help"></a> </div>
+              <div class="row"> <span class="left">Adults</span>
+                <input type="text" class="input2" value="2"  onblur="if(this.value=='') this.value='2'" onFocus="if(this.value =='2' ) this.value=''">
+              </div>
+              <div class="row"> <span class="left">Children</span>
+                <input type="text" class="input2" value="0"  onblur="if(this.value=='') this.value='0'" onFocus="if(this.value =='0' ) this.value=''">
+                <span class="pad_left1">(0-11 years)</span> </div>
+              <div class="wrapper"> <span class="right relative"><a href="#" class="button1"><strong>Search</strong></a></span> <a href="#" class="link1">More Options</a> </div>
+            </div>
+          </form>
+        </div>
+        <div class="tab-content" id="Rental">
+          <form id="form_3" action="#" method="post">
+            <div>
+              <div class="radio">
+                <div class="wrapper">
+                  <input type="radio" name="name2" checked>
+                  <span class="left">Avis</span>
+                  <input type="radio" name="name2">
+                  <span class="left">Europcar</span> </div>
+              </div>
+              <div class="row"> <span class="left">Rental location</span>
+                <input type="text" class="input">
+              </div>
+              <div class="row"> <span class="left">Pick-up</span>
+                <input type="text" class="input1" value="03.05.2011"  onblur="if(this.value=='') this.value='03.05.2011'" onFocus="if(this.value =='03.05.2011' ) this.value=''">
+                <input type="text" class="input2" value="12:00"  onblur="if(this.value=='') this.value='12:00'" onFocus="if(this.value =='12:00' ) this.value=''">
+              </div>
+              <div class="row"> <span class="left">Return</span>
+                <input type="text" class="input1" value="10.05.2011"  onblur="if(this.value=='') this.value='10.05.2011'" onFocus="if(this.value =='10.05.2011' ) this.value=''">
+                <input type="text" class="input2" value="12:00"  onblur="if(this.value=='') this.value='12:00'" onFocus="if(this.value =='12:00' ) this.value=''">
+              </div>
+              <div class="row_select"> <span class="left">Miles &amp; More</span>
+                <select>
+                  <option>no membership</option>
+                </select>
+              </div>
+              <div class="row_select">
+                <div class="pad_left1"> Country of residence<br>
+                  <div class="select1">
+                    <select>
+                      <option>&nbsp;</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="wrapper"> <span class="right relative"><a href="#" class="button1"><strong>Search</strong></a></span> </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </article>
+  <div id="slider"> <img src="images/banner1.jpg" alt=""> <img src="images/banner2.jpg" alt=""> <img src="images/banner3.jpg" alt=""> </div>
+</div>
+<div class="wrapper pad1">
+  <article class="col1">
+    <div class="box1">
+      <h2 class="top">Offers of the Week from UK</h2>
+      <div class="pad"> <strong>From Birmingham</strong><br>
+        <ul class="pad_bot1 list1">
+          <li> <span class="right color1">from GBP 143.-</span> <a href="book2.html">Zurich</a> </li>
+        </ul>
+        <strong>From London City</strong><br>
+        <ul class="pad_bot1 list1">
+          <li> <span class="right color1">from GBP 176.-</span> <a href="book2.html">Basel</a> </li>
+          <li> <span class="right color1">from GBP 109.-</span> <a href="book2.html">Geneva</a> </li>
+        </ul>
+        <strong>From London Heathrow</strong><br>
+        <ul class="pad_bot2 list1">
+          <li> <span class="right color1">from GBP 100.-</span> <a href="book2.html">Geneva</a> </li>
+          <li> <span class="right color1">from GBP 112.-</span> <a href="book2.html">Zurich</a> </li>
+          <li> <span class="right color1">from GBP 88.-</span> <a href="book2.html">Basel</a> </li>
+        </ul>
+      </div>
+      <h2>From Ireland To Switzerland</h2>
+      <div class="pad"> <strong>From Dublin</strong><br>
+        <ul class="pad_bot2 list1">
+          <li class="pad_bot1"> <span class="right color1">from EUR 122.-</span> <a href="book2.html">Zurich</a> </li>
+        </ul>
+      </div>
+    </div>
+  </article>
+  <article class="col2">
+    <h3>About Our Airlines<span>Template created by TemplateMonster.com team</span></h3>
+    <div class="wrapper">
+      <article class="cols">
+        <figure><img src="images/page1_img1.jpg" alt="" class="pad_bot2"></figure>
+        <p class="pad_bot1"><strong>Airlines is one of free website templates created by TemplateMonster.com team.</strong></p>
+        <p>This website template is optimized for 1024X768 screen resolution. It is also XHTML &amp; CSS valid. This website template has several pages: <a href="index.html">About</a>, <a href="offers.html">Offers</a>, <a href="book.html">Book</a>, <a href="services.html">Services</a>, <a href="safety.html">Safety</a>, <a href="contacts.html">Contacts</a>.</p>
+      </article>
+      <article class="cols pad_left1">
+        <figure><img src="images/page1_img2.jpg" alt="" class="pad_bot2"></figure>
+        <p class="pad_bot1"><strong>This Airlines Template goes with two packages.</strong></p>
+        <p>With PSD source files and without them. PSD source files are available for free for the registered members of Templates.com. The basic package (without PSD source files) is available for anyone without registration).</p>
+      </article>
+    </div>
+    <a href="#" class="button1"><strong>Read More</strong></a> </article>
 </div>
--- application/views/template.php
+++ application/views/template.php
@@ -14,29 +14,67 @@ if (!defined('APPPATH'))
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>{pagetitle}</title>
-        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-        <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
-    </head>
-    <body>
-        <div class="container">
-            <div class="navbar">
-                <div class="navbar-inner">
-                    <a class="brand" href="/"><img src="/assets/images/logo.png"/></a>
-                    {menubar} </div>
-            </div>           
-            <div id="content">
-                <h1>{pagetitle}</h1>
-                {content}
-            </div>
-            <div id="footer" class="span12">
-                Copyright &copy; 2015-2017,  <a href="mailto:someone@somewhere.com">Me</a>.
-            </div>
+<head>
+    <title>AirLines</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/reset.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all">
+    <link rel="stylesheet" href="assets/css/layout.css" type="text/css" media="all">
+    <script type="text/javascript" src="assets/js/jquery-1.5.2.js" ></script>
+    <script type="text/javascript" src="assets/js/cufon-yui.js"></script>
+    <script type="text/javascript" src="assets/js/cufon-replace.js"></script>
+    <script type="text/javascript" src="assets/js/Cabin_400.font.js"></script>
+    <script type="text/javascript" src="assets/js/tabs.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.jqtransform.js" ></script>
+    <script type="text/javascript" src="assets/js/jquery.nivo.slider.pack.js"></script>
+    <script type="text/javascript" src="assets/js/atooltip.jquery.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="js/html5.js"></script>
+    <style type="text/css">.main, .tabs ul.nav a, .content, .button1, .box1, .top { behavior:url("../js/PIE.htc")}</style>
+    <![endif]-->
+</head>    
+<body>
+    <header>
+    <div class="wrapper">
+      <h1><a href="index.html" id="logo">AirLines</a></h1>
+      <span id="slogan">Fast, Frequent &amp; Safe Flights</span>
+      <nav id="top_nav">
+        <ul>
+          <li><a href="index.html" class="nav1">Home</a></li>
+          <li><a href="#" class="nav2">Sitemap</a></li>
+          <li><a href="contacts.html" class="nav3">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+    <nav>
+      <ul id="menu">
+        <li id="menu_active"><a href="index.html"><span><span>About</span></span></a></li>
+        <li><a href="offers.html"><span><span>Offers</span></span></a></li>
+        <li><a href="book.html"><span><span>Book</span></span></a></li>
+        <li><a href="services.html"><span><span>Services</span></span></a></li>
+        <li><a href="safety.html"><span><span>Safety</span></span></a></li>
+        <li class="end"><a href="contacts.html"><span><span>Contacts</span></span></a></li>
+      </ul>
+    </nav>
+    </header>
+    
+    <section id="content">
+        {content}
+    </section>
+    
+    <footer>
+        <div class="wrapper">
+          <ul id="icons">
+            <li><a href="#" class="normaltip"><img src="assets/images/icon1.jpg" alt=""></a></li>
+            <li><a href="#" class="normaltip"><img src="assets/images/icon2.jpg" alt=""></a></li>
+            <li><a href="#" class="normaltip"><img src="assets/images/icon3.jpg" alt=""></a></li>
+            <li><a href="#" class="normaltip"><img src="assets/images/icon4.jpg" alt=""></a></li>
+            <li><a href="#" class="normaltip"><img src="assets/images/icon5.jpg" alt=""></a></li>
+            <li><a href="#" class="normaltip"><img src="assets/images/icon6.jpg" alt=""></a></li>
+          </ul>
+          <div class="links">Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved</div>
         </div>
-        <script src="/assets/js/jquery-1.11.1.min.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
-    </body>
+    </footer>
+</body>
 </html>
