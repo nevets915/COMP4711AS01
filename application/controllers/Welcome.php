99ccf724b5012795ccb6e832a438fdebf6762306
Shawn <sdf1342@gmail.com>
2017-10-08 19:05:53 -0700

    Welcome.php hompage.php updated

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -14,17 +14,35 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+        $this->load->model('Airline');
 
         $fleets =  $this -> fleet_model -> all();
         $flights =  $this -> flights_model -> all();
+        $airlines = $this -> Airline -> all();
 
         $fleet_count = count($fleets);
         $flight_count = count($flights);
+
+
+        foreach ($airlines as $key => $value){
+            if($value['id'] == 'bluebird'){
+                $base = $value['base'];
+
+                $destinations = array(
+                    '1' => array('dest' => $value['dest1']),
+                    '2' => array('dest' => $value['dest2']),
+                    '3' => array('dest' => $value['dest3'])
+                );
+            }
+        }
+
 
 		// this is the view we want shown
 		$this->data['pagebody'] = 'homepage';
         $this->data['fleet_count'] = $fleet_count;
         $this->data['flight_count'] = $flight_count;
+        $this->data['destinations'] = $destinations;
+        $this->data['base_airport'] = $base;
 		$this->render();
 	}
 }
